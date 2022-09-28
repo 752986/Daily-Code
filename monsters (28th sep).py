3952bde4c877e29dcf9b48cb20eae4f2f1cd0cd1
752986 <112513827+752986@users.noreply.github.com>
2022-09-28 14:47:17 -0600

    finish monster petting sim

--- monsters (28th sep).py
+++ monsters (28th sep).py
@@ -6,45 +6,121 @@ pygame.init
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("monster gen with inheritance")
 
-monsterlist = []  # a list to hold all the monsters the monstergen makes
-
 # function definition for circular collision-------------------------------------------
-def CircularCollision(x1, y1, x2, y2):
+def CircularCollision(x1, y1, x2, y2, radius = 50): #polymorphism! wow!
     x1 += 16  # adjust so center is not top left corner
     y1 += 16
-    if math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)) < 50:
+    if math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)) < radius:
         return True
     else:
         return False
 
+class GameObject():
+    x: float
+    y: float
+    image: pygame.surface.Surface
+
+    def update(self, clock):
+        pass
+
+    def draw(self, surface = screen):
+        pass
+
+    def should_delete(self):
+        pass
+
+monsterlist: list[GameObject] = []  # a list to hold all the monsters the monstergen makes
+
+
 # parent class-------------------------------------------------------------------------
+class Heart(GameObject):
+    image = pygame.transform.scale(pygame.image.load("./res/fcd0300deb4fd92735c20a6ea91ec1ca.png"), (20, 20))
+
+    def __init__(self, x: float, y: float, vx: float, vy: float):
+        self.x = x
+        self.y = y
+        self.vx = (random.random() - 0.5) * vx
+        self.vy = vy
+        self.vxscale = vx * 0.3
+
+    def update(self, clock):
+        self.x += self.vx
+        self.y += self.vy
+        self.vx += (random.random() - 0.5) * self.vxscale
+
+    def draw(self, surface = screen):
+        surface.blit(self.image, (self.x, self.y))
+
+    def should_delete(self) -> bool:
+        return not (screen.get_rect().colliderect(((self.x, self.y), (20, 20))))
 
 
-class Monster():
+class Hand(GameObject):
+    HAND_SPEED = 300
+    image1 = pygame.transform.scale(pygame.image.load("./res/frame_0_delay-0.05s.gif"), (50, 50))
+    image2 = pygame.transform.scale(pygame.image.load("./res/frame_2_delay-0.05s.gif"), (50, 50))
+    
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.image = self.image1
+        self.image.set_colorkey((255, 255, 255))
+        self.image = self.image2
+        self.image.set_colorkey((255, 255, 255))
+        self.accum = 0.0
 
-    def __init__(self, x: int, y: int, image: str, size: float):  # constructor
-        self.xpos: int = x
-        self.ypos: int = y
-        self.health: int = 1000
+    def update(self, clock):
+        self.x = pygame.mouse.get_pos()[0] - 25
+        self.y = pygame.mouse.get_pos()[1] - 20
+
+        self.accum += clock.get_time()
+
+        if not pygame.mouse.get_pressed()[0]:
+            self.image = self.image2
+        else:
+            if self.accum % self.HAND_SPEED > self.HAND_SPEED / 2:
+                self.image = self.image1
+            else:
+                self.image = self.image2
+
+    def draw(self, surface = screen):
+        surface.blit(self.image, (self.x, self.y))
+
+    def should_delete(self) -> bool:
+        return False
+    
+class Monster(GameObject):
+    def __init__(self, x: int, y: int, image: str, size: float): # constructor
+        self.x: int = x
+        self.y: int = y
+        self.health: int = random.randint(400, 3000)
         self.alive: bool = True
-        self.size = size
         self.image: pygame.surface.Surface = pygame.image.load(image)
-        self.image = pygame.transform.scale(self.image, (self.image.get_width() * self.size, self.image.get_height() * self.size))
+        self.bounds: pygame.rect.Rect = pygame.rect.Rect((self.x, self.y), (self.image.get_width() * size, self.image.get_height() * size))
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * size, self.image.get_height() * size))
 
+    def emitParticle(self):
+        h = Heart(pygame.mouse.get_pos()[0] + random.randint(-20, 20), pygame.mouse.get_pos()[1] + random.randint(-20, 20), 2, -4)
+        monsterlist.append(h)
 
     def getClicked(self):
-        if CircularCollision(self.xpos, self.ypos, pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]) == True and self.alive:
-            print("hit!")
+        if self.alive and pygame.mouse.get_pressed()[0] and self.bounds.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]):
+            print(random.choice(["💖", "🥰", "😊", "✨", "\n"]), end="")
             self.health -= 10
+            self.emitParticle()
 
-    def ded(self):
+    def update(self, clock):
+        self.getClicked()
         if self.health <= 0 and self.alive:
-            print("I'm ded.")
+            print("\n Bye for now!")
             self.alive = False
+
+    def should_delete(self) -> bool:
+        return self.health <= 0
 
     def draw(self, surface: pygame.surface.Surface = screen):
         if self.alive:  # dont draw ded monsters
-            surface.blit(self.image, (self.xpos, self.ypos))
+            surface.blit(self.image, (self.x, self.y))
 
 
 # child class--------------------------------------------------------------------------
@@ -52,6 +128,7 @@ class Creeper(Monster):
     def __init__(self, x: int, y: int):
         # call constructor of parent class
         super().__init__(x, y, "./res/GUEST_ac7896ab-ec5c-4e4b-b428-b1c563084cde.jpg", 0.2)
+        self.image.set_colorkey((255, 255, 255))
         
 
 # add more child classes here!
@@ -95,6 +172,7 @@ def MonsterGen():
 # use monstergen to make the others
 for i in range(20):
     MonsterGen()
+monsterlist.append(Hand())
 
 # creates game screen and caption
 
@@ -103,6 +181,7 @@ for i in range(20):
 doExit = False  # variable to quit out of game loop
 clock = pygame.time.Clock()  # sets up a game clock to regulate game speed
 pos = pygame.mouse.get_pos()
+pygame.mouse.set_visible(False)
 click = False
 
 #BEGIN GAME LOOP######################################################
@@ -110,6 +189,7 @@ while not doExit:
 
     # input section-----------------------------------
     clock.tick(60)  # FPS (frames per second)
+
 
     # pygame's way of listening for events (key presses, mouse clicks, etc)
     for event in pygame.event.get():
@@ -126,9 +206,10 @@ while not doExit:
         pos = pygame.mouse.get_pos()
 
     # physics section----------------------------------
-    for i in range(len(monsterlist)):
-        monsterlist[i].getClicked()
-        monsterlist[i].ded()
+    for o in monsterlist:
+        o.update(clock)
+        if o.should_delete():
+            monsterlist.remove(o)
 
     # render section-----------------------------------
 
