93dc96c2278cf5f633ec62505cbab4d3234550e2
752986 <112513827+752986@users.noreply.github.com>
2022-09-29 13:59:17 -0600

    Create among us (29th sep).py
    ran out of time :(

---
+++ among us (29th sep).py
@@ -0,0 +1,95 @@
+import pygame
+from pygame import mouse
+from pygame.rect import Rect
+from pygame.math import Vector2
+from pygame.color import Color
+from pygame.surface import Surface
+
+pygame.init()
+screen = pygame.display.set_mode((1200, 900))
+
+
+# Class definitions:
+
+class GameObject:
+	pos :Vector2
+	color :Color
+	should_delete :bool
+
+	def update(self, delta :float):
+		pass
+
+	def draw(self, surface :Surface):
+		pass
+
+class Target(GameObject):
+	RED = 0
+	YELLOW = 1
+	GREEN = 2
+	BLUE = 3
+
+	COLORS = [
+		Color(200, 100, 100),
+		Color(200, 200, 100),
+		Color(100, 200, 100),
+		Color(100, 100, 200)
+	]
+
+	bounds :Rect
+	colorname :int
+
+	def __init__(self, pos :tuple[float, float], color :int, size :tuple[float, float] = (200, 100)):
+		self.pos = Vector2(pos)
+		self.colorname = color
+		self.color = self.COLORS[color]
+		self.bounds = Rect(pos, size)
+
+	def update(self, delta :float):
+		pass
+
+
+	def draw(self, surface :Surface):
+		pygame.draw.rect(surface, self.color, self.bounds, 0, 4)
+
+
+# Game setup:
+clock = pygame.time.Clock()
+objects :list[GameObject] = []
+objects.append(Target((100, 100), Target.BLUE))
+
+startpoint :Vector2 | None = None
+startcolor :int | None
+
+just_pressed = False
+
+# Main loop:
+running = True
+while running:
+	clock.tick(75)
+
+	# Input:
+	just_pressed = False
+	for e in pygame.event.get():
+		if e.type == pygame.QUIT:
+			running = False
+		elif e.type == pygame.MOUSEBUTTONDOWN:
+			just_pressed = True
+		elif e.type == pygame.MOUSEBUTTONUP:
+			startpoint = None
+
+	# Update:
+	for o in objects:
+		o.update(clock.get_time() / 1000)
+
+		if just_pressed and type(o) is Target and o.bounds.collidepoint(mouse.get_pos()):
+			startpoint = Vector2(mouse.get_pos())
+
+	# Draw:
+	screen.fill(Color(20, 20, 20))
+	for o in objects:
+		o.draw(screen)
+
+	if startpoint != None:
+		pygame.draw.line(screen, Color(255, 255, 255), startpoint, mouse.get_pos(), 4)
+	
+	pygame.display.flip()
