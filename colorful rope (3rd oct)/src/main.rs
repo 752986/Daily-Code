b5cbcdf0982e8cf1cfbf6d8225888d4194d71877
752986 <112513827+752986@users.noreply.github.com>
2022-10-04 14:07:22 -0600

    actually works

--- colorful rope (3rd oct)/src/main.rs
+++ colorful rope (3rd oct)/src/main.rs
@@ -1,21 +1,40 @@
+use std::ops::Index;
+
 struct Solution {}
 
 impl Solution {
 	pub fn min_cost(colors: String, needed_time: Vec<i32>) -> i32 {
-		let mut chars = colors.chars().collect::<Vec<char>>();
+		// part 1: find the lengths of each span of chars
 		let mut cost = 0;
-		loop {
-			let mut zipped = chars.iter().zip(&needed_time);
-			let mut prev = zipped.next().unwrap();
-			for (i, curr) in zipped.enumerate() {
-				if curr.0 == prev.0 {
-					if curr.1 < prev.1 {
-						zipped.remove(i);
-					}
-				}
+		let mut chars = colors.chars();
+		let mut ranges = Vec::new();
+		ranges.push((chars.next().unwrap(), 1));
+		for c in chars {
+			if c != ranges.last().unwrap().0 {
+				ranges.push((c, 1));
+			} else {
+				ranges.last_mut().unwrap().1 += 1;
 			}
 		}
-		return cost;
+		
+		// part 2: remove all but the max cost values in each range
+		let mut offset = 0;
+		for r in ranges {
+			let slice = &needed_time[offset..(offset + r.1)];
+			let max = slice.iter().max().unwrap();
+			let mut skipped = false;
+			for t in slice.iter() {
+				if !skipped && t == max {
+					skipped = true;
+					continue
+				} else {
+					cost += t;
+				}
+			}
+
+			offset += r.1;
+		}
+		return cost
 	}
 }
 
