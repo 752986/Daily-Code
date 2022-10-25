0779dc180770ac6997ee46a4e3581a4dac29584b
752986 <112513827+752986@users.noreply.github.com>
2022-10-24 19:29:42 -0600

    Update with new understanding!

--- mergesort.py
+++ mergesort.py
@@ -1,27 +1,38 @@
 import random
 import time
 
-def mergesort(a: list[float]):
-	a = a.copy()
-	if not len(a) == 1:
-		a1 = a[0 : len(a)//2]
-		a1 = mergesort(a1)
+def mergesort(input: list[float]) -> list[float]:
+	if len(input) == 1:
+		return input
+	a = input[0 : len(input)//2]
+	b = input[len(input)//2 : ]
+	sorted_a = mergesort(a)
+	sorted_b = mergesort(b)
 
-		a2 = a[len(a)//2 : len(a)]
-		a2 = mergesort(a2)
+	result: list[float] = []
+	a_index = 0
+	b_index = 0
+	while a_index < len(sorted_a) and b_index < len(sorted_b):
+		if sorted_a[a_index] < sorted_b[b_index]:
+			result.append(sorted_a[a_index])
+			a_index += 1
+		else:
+			result.append(sorted_b[b_index])
+			b_index += 1
+	result = result + sorted_a[a_index : ] + sorted_b[b_index : ]
 
-		a.clear()
-		a.extend(a1)
-		a.extend(a2)
-		a.sort()
-		return a
-	else:
-		return a
+	return result
 
-test: list[float] = [random.randint(0, 10000) for _ in range(10_000_000)]
+test: list[float] = [random.randint(0, 10000) for _ in range(1_000_000)]
 
 start = time.perf_counter()
 result = mergesort(test)
 duration = time.perf_counter() - start
 
-print("time: {}".format(duration))
+print("mergesort time: {}".format(duration))
+
+start = time.perf_counter()
+result = test.sort()
+duration = time.perf_counter() - start
+
+print("builtin time: {}".format(duration))
