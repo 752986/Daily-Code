0681fa50a16aee2a0008e9ebb9692c798963c4c8
Dec <112513827+752986@users.noreply.github.com>
2023-03-14 14:50:17 -0600

    js is garbage

---
+++ 2nd semester/simon/index.js
@@ -0,0 +1,81 @@
+const colors = ["red", "green", "blue", "yellow"]
+
+function main() {
+
+	let sequence = [colors[Math.random()]]
+
+	let waiting = true
+	let running = true
+	addColor(sequence)
+	// while (running) {
+
+		// while (waiting) {} // wait until a button is pressed
+	// }
+	// setInterval(addColor.bind(addColor, sequence), 1000)
+
+	// sequence.push(colors[getRandomInt(0, 4)])
+	// displaySequence(sequence)
+
+	// addColor(sequence)
+}
+
+function addColor(sequence) {
+	sequence.push(colors[getRandomInt(0, 4)])
+
+	displaySequence(sequence)
+}
+
+// taken from MDN docs
+function getRandomInt(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min) + min)
+}
+
+function displaySequence(sequence, callback, time = 1000, index = 0) {
+	const red = document.getElementById("red")
+	const green = document.getElementById("green")
+	const blue = document.getElementById("blue")
+	const yellow = document.getElementById("yellow")
+
+	red.style.backgroundColor = '#db6962'
+	green.style.backgroundColor = '#85ad58'
+	blue.style.backgroundColor = '#6991d5'
+	yellow.style.backgroundColor = '#cca738'
+
+	setTimeout(callback, time * sequence.length)
+
+	
+	const item = sequence[i];
+	switch (item) {
+		case "red":
+			red.style.backgroundColor = '#ffffff'
+			setTimeout(() => {
+				displaySequence(sequence, time, index + 1)
+			}, time)
+			break
+		case "green":
+			green.style.backgroundColor = '#ffffff'
+			setTimeout(() => {
+				displaySequence(sequence, time, index + 1)
+			}, time)
+			break
+		case "blue":
+			blue.style.backgroundColor = '#ffffff'
+			setTimeout(() => {
+				displaySequence(sequence, time, index + 1)
+			}, time)
+			break
+		case "yellow":
+			yellow.style.backgroundColor = '#ffffff'
+			setTimeout(() => {
+				displaySequence(sequence, time, index + 1)
+			}, time)
+			break
+	}	
+}
+
+function wait(secs) {
+	const start = Date.now()
+	while (Date.now() - start < secs * 100) {}
+}
